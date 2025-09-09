bf1bf368c323a6d1e37a5c9e42171b93b92a0d5f
Laasya6429 <laasya6429@gmail.com>
2025-09-09 12:13:21 +0530

    changes made in AKS-TF

--- AKS-TF/backend.tf
+++ AKS-TF/backend.tf
@@ -5,11 +5,4 @@ terraform {
     container_name       = "tfstate"
     key                  = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/AKS-TF/terraform.tfstate"
   }
-  required_version = ">=0.13.0"
-  required_providers {
-    azurerm = {
-      version = ">= 3.0.0"
-      source  = "hashicorp/azurerm"
-    }
-  }
 } 
